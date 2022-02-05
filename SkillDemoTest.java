9deb9bf049fd39b33e572c99e8490b31dcfc7b2d
aaadit24 <97692709+aaadit24@users.noreply.github.com>
2022-02-04 16:30:25 -0800

    d

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest{
     @Test
 	public void multiplication(){
-		assertEquals(12, SkillDemo.multiply(1,2));
+		assertEquals(2, SkillDemo.multiply(3,4));
 	}
 }  
